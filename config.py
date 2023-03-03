581ed998deb0f78af784c0269453d6a3859c823e
CUI <ccyiyili@163.com>
2023-03-03 12:04:45 +0800

    Update config.py
    错误忽略

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ def get_root():
 
 
 def read_file(path):
-    with open(path, mode='r', encoding='utf-8') as f:
+    with open(path, mode='r', encoding='utf-8', errors='ignore') as f:
         return f.read()
 
 
